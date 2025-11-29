8950ea72a3a9d235638b3c2aa6266e0408d294d7
Ogundowole Raheem <oluwatayoadeyemi@yahoo.com>
2025-11-29 13:07:43 +0100

    Auth

--- auth/login.php
+++ auth/login.php
@@ -1,15 +1,28 @@
 <?php
+
 require_once __DIR__ . '/../components/mainhead.php';
+
+use Includes\Security\CSRF;
+use Model\Utility;
+
+$utility_instance = new Utility($db);
 ?>
+
 <body>
     <div class="container mt-5 bg-light rounded shadow" style="margin-top: 50px; max-width: 500px;">
         <h2 class="text-center baskervville-sc-regular mb-4">Welcome Back</h2>
-        
-        <form action="process/process_login.php" method="post">
+
+        <?php echo $utility_instance->displayAlertMessage(); ?>
+
+        <form action="<?php echo CONTROLLER_URL;?>auth.php" method="post">
+            <?php echo CSRF::csrfField(); ?>
+
             <div class="mb-3">
                 <label for="loginEmail" class="form-label">Email or Username</label>
                 <input type="text" name="email" class="form-control" id="loginEmail" 
-                    placeholder="Enter your email or username">
+                    placeholder="Enter your email or username" 
+                    value="<?php echo $utility_instance->returnFormInput('email');?>"
+                >
             </div>
 
             <div class="mb-3">
@@ -26,7 +39,7 @@ require_once __DIR__ . '/../components/mainhead.php';
                 <a href="#">Forgot password?</a>
             </div>
 
-            <button type="submit" name="btn" class="btn btn-success">
+            <button type="submit" name="corper_login" class="btn btn-success">
                 <i class="fa fa-paper-plane"></i> Login
             </button>
         </form>
